5f91beefc1ff1ea4f82675b99e647dc48472553e
ros45667 <rosmireji266@gmail.com>
2026-03-18 23:18:00 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 50
+
+typedef struct {
+    char pid[10];
+    int at, bt;
+    int ct, wt, tat;
+    int completed;
+} Process;
+
+int main() {
+    int n;
+    Process p[MAX];
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    // Input
+    for(int i = 0; i < n; i++) {
+        scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
+        p[i].completed = 0;
+    }
+
+    int time = 0, completed = 0;
+
+    while(completed < n) {
+        int idx = -1;
+        int min_bt = 1e9;
+
+        // Find process with minimum BT among arrived processes
+        for(int i = 0; i < n; i++) {
+            if(p[i].at <= time && p[i].completed == 0) {
+                if(p[i].bt < min_bt) {
+                    min_bt = p[i].bt;
+                    idx = i;
+                }
+            }
+        }
+
+        if(idx == -1) {
+            time++; // CPU idle
+        } else {
+            // Execute process
+            time += p[idx].bt;
+            p[idx].ct = time;
+            p[idx].tat = p[idx].ct - p[idx].at;
+            p[idx].wt = p[idx].tat - p[idx].bt;
+            p[idx].completed = 1;
+            completed++;
+        }
+    }
+
+    float total_wt = 0, total_tat = 0;
+
+    printf("\nWaiting Time:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%s %d\n", p[i].pid, p[i].wt);
+        total_wt += p[i].wt;
+    }
+
+    printf("Turnaround Time:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%s %d\n", p[i].pid, p[i].tat);
+        total_tat += p[i].tat;
+    }
+
+    printf("Average Waiting Time: %.2f\n", total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+
+    return 0;
+}
